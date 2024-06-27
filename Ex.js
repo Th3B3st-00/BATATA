0f5018058b11d1165adb0876e138cc024005d446
Th3B3st-00 <167141622+Th3B3st-00@users.noreply.github.com>
2024-06-26 21:40:36 -0300

    Update Ex.js

--- Ex.js
+++ Ex.js
@@ -3,21 +3,28 @@ let nome = 'João'
 
 console.log(nome)
 
+console.log("\n")
 //Ex 02
 let a =5, b = 10, n
 
 n = a + b
 console.log(n)
 
+console.log("\n")
+
 //Ex 03
 let primeiroNome = 'Josefa', ultimoNome = 'Silva'
 
 console.log(primeiroNome, ultimoNome)
 
+console.log("\n")
+
 //Ex 04
 let pessoa = {nome: 'Roberto', idade: 17, endereço: 'Rua mamaco', telefone: 99998888}
 
 console.log(pessoa)
+
+console.log("\n")
 
 //Ex 01 -------------VETORES-----------------
 let frutas = ['maça', 'banana', 'laranja']
@@ -25,16 +32,22 @@ let frutas = ['maça', 'banana', 'laranja']
 frutas[3] = 'morango'
 console.log(frutas)
 
+console.log("\n")
+
 //Ex 02
 let numeros = [1, 2, 3, 4 ,5]
 
 console.log(numeros[0], numeros[4])
+
+console.log("\n")
 
 //Ex 03
 let cores = ['vermelho', 'verde', 'azul']
 
 for (let i = 0; i <= 2; i++)
 console.log(cores[i])
+
+console.log("\n")
 
 //Ex 01 ---------ESTRUTURAS DE DECISÃO-------
 let idade = 15
@@ -45,6 +58,8 @@ if(idade < 18){
 } else {
     console.log('Você é maior de idade')
 }
+
+console.log("\n")
 
 //Ex 02
 let nota = 6
@@ -58,6 +73,8 @@ if(nota >= 5 & nota<7){
 if(nota < 5){
     console.log('\nReprovado') 
     }
+    
+    console.log("\n")
 
 //Ex 03
 let nomedoDia, diadaSemana = 3
@@ -87,9 +104,13 @@ switch(diadaSemana){
 }
 console.log(nomedoDia)
 
+console.log("\n")
+
 //Ex 01 --------LAÇOS DE REPETIÇÃO----------
 for(let i = 0; i <= 10; i++)
     console.log(i)
+    
+    console.log("\n")
     
 //Ex 02
 let contador = 1
@@ -99,6 +120,8 @@ while(contador <= 5){
     contador++
 }
 
+console.log("\n")
+
 //Ex 03
 let numero = 0
 
@@ -107,6 +130,8 @@ do {
     numero++
 } while(numero <= 3)
 
+console.log("\n")
+
 //Ex 01 -------COMBINANDO CONCEITOS-------
 let valores = [10, 20, 30, 40, 50], soma = 0
 
@@ -114,12 +139,60 @@ for(let i = 0; i < 5; i++)
 soma += valores[i]
 console.log(soma)
 
+console.log("\n")
+
 //Ex 02
-let numeros = [56, 2, 76]
-let maiorNumero = numeros[0]
+let num = [56, 2, 76]
+let maiorNumero = num[0]
 
 for(let i = 1; i <= 3; i++){
-    if(numeros[i] > maiorNumero)
-    maiorNumero = numeros[i]
+    if(num[i] > maiorNumero)
+    maiorNumero = num[i]
 }
 console.log(maiorNumero)
+
+console.log("\n")
+
+//Ex 03
+let nu = [2,35,5]
+let menorNumero = nu[0]
+
+for(let i = 0; i <= 3; i++){
+    if(nu[i] < menorNumero)
+    menorNumero = nu[i]
+}
+console.log(menorNumero)
+
+console.log("\n")
+
+//Ex 04
+let fatorial = 4
+let resultado = 1
+
+for(let i = 1; i <= fatorial; i++){
+    resultado = resultado * i
+}
+ console.log(resultado)
+ 
+ console.log("\n")
+ 
+ //Ex 05
+ let nm = [1, 2, 3, 4, 5, 6, 7, 8 ,9 , 10]
+ 
+ for(let i = 0; i <= nm.lenght; i++){
+     if(nm[i] % 2==0){
+         console.log("par" + nm)
+     } else {
+         console.log("impar" + nm)
+     }
+ }
+ 
+ let texto = 'masqueico'
+ 
+ let contado = 0
+ 
+ for(let i = 0; i < texto.lenght; i++){
+     if(texto[i] === 'a' || texto[i] === 'e' || texto[i] === 'i' || texto[i] === 'o' || texto[i] === 'u'){
+     }
+ }
+console.log(contado + 'é o número de vogáis')
